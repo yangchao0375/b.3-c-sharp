3fd8da37da8cde9e7f384dc1bf0437dc685bd3ff
3253099643qq.com <3253099643@qq.com>
2021-03-13 16:41:47 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b._3_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int max;
+            int[] t = new int[101];
+            for (int i = 0; i <= 100; i++)
+            {
+                t[i] = i;
+            }
+            max = 100;
+            for (int i = 2; i * i < 100; i++)
+            {
+                for (int j = 100; j > i; j--)
+                {
+                    /*if (i * i > max)
+                    {
+                        break;
+                    }*/
+                    if (t[j] % i == 0)
+                    {
+                        t[j] = 0;
+                        max = t[j - 1];
+                    }
+                }
+            }
+            for (int i = 2; i <= 100; i++)
+            {
+                if (t[i] != 0)
+                {
+                    Console.WriteLine(t[i]);
+                }
+            }
+        }
+    }
+}
+
